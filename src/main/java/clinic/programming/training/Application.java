d2760c4743844d73a5dd74de5be527df25f07196
tovanhieu <tovanhieu1996.hl@gmail.com>
2022-10-16 16:14:03 +0700

    Changig Application java file

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,16 @@
 package clinic.programming.training;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
+	
+	public void greet() {
+		List<String> greetings = new ArrayList<String>();
+		greetings.add("Hello");
+		for(String greeting : greetings){
+			System.out.println("Greeting: " + greeting);
+		}
+	}
     
     public Application() {
         System.out.println ("Inside Application");
@@ -9,6 +19,7 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+	    Application app = new Application();
+		app.greet();
     }
 }
